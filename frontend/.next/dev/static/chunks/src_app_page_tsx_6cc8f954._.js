1f0fd2ddce591d9e57c734aa8531995a545967d6
w1zll <105282164+w1zll@users.noreply.github.com>
2026-03-16 22:43:33 +0500

    Delete frontend/.next directory

--- frontend/.next/dev/static/chunks/src_app_page_tsx_6cc8f954._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_@tanstack_e11db6bf._.js",
-  "static/chunks/src_e2966f08._.js"
-],
-    source: "dynamic"
-});
--- frontend/.next/dev/static/chunks/src_app_page_tsx_e6fd7144._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_dbf8079e._.js",
-  "static/chunks/src_entities_ad_42ff5aa4._.js"
-],
-    source: "dynamic"
-});
