687ec4ce5049d4aa406238666736b43655dbf927
w1zll <filin.vitalik2244@mail.ru>
2026-03-16 22:58:54 +0500

    removed .next

--- frontend/.next/dev/server/chunks/ssr/_next-internal_server_app_ads_[id]_page_actions_19e3769b.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/ads/[id]/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_ads_%5Bid%5D_page_actions_19e3769b.js.map
--- frontend/.next/dev/static/chunks/src_app_page_tsx_0958b1ac._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_7ecc3aed._.js"
-],
-    source: "dynamic"
-});
